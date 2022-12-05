003105418248ed64d46755ecfc7fa57e9756a351
José Anderson <jose.andersonjr2@gmail.com>
2022-12-05 20:35:06 -0300

    Adicionada função criarUsuario()

--- projeto/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
+++ projeto/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import br.com.criandoapi.projeto.model.Usuario;
@@ -20,6 +21,12 @@ public class UsuarioController{
 	public List<Usuario> listaUsuarios() {
 		
 		return (List<Usuario>) dao.findAll();
+	}
+	
+	@GetMapping
+	public Usuario criarUsuario(@RequestBody Usuario usuario) {
+		Usuario usuarioNovo = dao.save(usuario);
+		return usuarioNovo;
 	}
 
 }
